a3eeb877e5274324c8797d0ad2dceb8f4e075db5
luqqy <luckyoula@gmail.com>
2020-02-23 10:04:32 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-$
+$(document).ready(function(){
+    $("#dev-image").click(function(){
+        $("#dev-image").slidedDown('1500').hide('1000');
+        $("#dev").show('1500')
+    });
+    $("dev").click
+
+});
