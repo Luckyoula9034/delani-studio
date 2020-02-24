c6b5454389e01b99c574dee783e981592a302006
luqqy <luckyoula@gmail.com>
2020-02-24 17:38:51 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,32 @@
 $(document).ready(function(){
-    $("#dev-image").click(function(){
-        $("#dev-image").slidedDown('1500').hide('1000');
-        $("#dev").show('1500')
+    $("#development-image").click(function(){
+      $("#development-image").slideDown('1500').hide('1000');
+      $("#development").show('1500');
     });
-    $("dev").click
-
-});
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#development-image").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#design-image").click(function(){
+      $("#design-image").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design-image").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#product-image").click(function(){
+      $("#product-image").slideDown('1500').hide('1000');
+      $("#product").show('1500');
+    });
+    $("#product").click(function(){
+      $("#product").slideUp('1500');
+      $("#product-image").slideDown('1500');
+    });
+  });
